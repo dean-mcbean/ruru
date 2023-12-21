14201f67b81e64fd2b61cbe5cd4dec394f4dd1ed
dean-mcbean <dean.w.mcbean@gmail.com>
2023-12-21 14:52:15 +1300

    woo

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,40 @@
 var express = require('express');
 const handlePullRequestEvent = require('../webhook_handlers/github/pull_requests');
+const { addReviewerToPullRequest } = require('../webhook_handlers/slack/pull_requests');
 var router = express.Router();
 
 /* POST from git. */
 router.post('/', async (req, res, next) => {
+  res.status(400)
 
   // Pass to relevant webhook handler
   if (req.body.pull_request) {
     await handlePullRequestEvent(req.body)
+    res.status(200)
   }
   
-  res.status(200).send('OK');
+  res.send();
+});
+
+/* POST from slack. */
+router.post('/slack/', async (req, res, next) => {
+  res.status(400)
+  const data = JSON.parse(req.body.payload)
+
+  // Pass actions to relevant webhook handler
+  console.log(data);
+  data.actions.forEach(action => {
+    switch (action.action_id) { 
+
+      // PULL REQUESTS
+      case 'pull_request.add_reviewer': 
+        addReviewerToPullRequest(action, data); 
+        res.status(200);
+        break;
+    }
+  })
+  
+  res.send();
 });
 
 module.exports = router;
--- webhook_handlers/github/pull_requests.js
+++ webhook_handlers/github/pull_requests.js
@@ -3,38 +3,104 @@ const sendMessage = require('../../slack_usecases/send_message');
 const updateMessage = require('../../slack_usecases/update_message');
 
 
-function generateMessageContent(data) {
+function sIf(num) {
+    // Returns an 's' if num is greater than 1 i.e. warrants a plural
+    return num > 1 ? 's' : ''
+}
 
-    // Determine Status Emoji
-    var status_emoji = ':eight_spoked_asterisk:';
-    if (data.pull_request.merged_at) {
-        // It's been merged!
-        status_emoji = ':merged:';
-    } else if (data.action == 'closed') {
-        // Closed and NOT merged
-        status_emoji = ':x:'
-    }
+function generateDescription(data) {
+    let description = [];
+    if (data.pull_request.additions) 
+        description.push(`${data.pull_request.additions} Addition${sIf(data.pull_request.additions)}`)
+    if (data.pull_request.deletions)
+        description.push(`${data.pull_request.deletions} Deletion${sIf(data.pull_request.deletions)}`)
+    if (data.pull_request.changed_files)
+        description.push(`${data.pull_request.changed_files} Changed File${sIf(data.pull_request.changed_files)}`)
+    return description.join(', ')
+}
+
+async function generateMessageContentForPullRequest(data) {
+    // Fetch info on this PR's status
+    const prStatuses = await storage.getItem('pr_status');
+    const pr_status = prStatuses[data.pull_request.id] ?? {};
+
 
     const creator = data.pull_request.user.login;
     const sender = data.sender.login; // The person who did this data.action
+
+    // Determine Status Emoji
+    var status = 'Open';
+    if (data.pull_request.merged_at) {
+        // It's been merged!
+        status = 'Merged';
+    } else if (data.pull_request.state == 'closed') {
+        // Closed and NOT merged
+        status = 'Closed';
+    }
+
+    // Link to repo
     const repo_url = `<${data.pull_request.head.repo.html_url}|${data.pull_request.head.repo.name}>`;
 
-    // Context Items - for PR history
-    const history_items = [
-        {
-            "text": (data.action != 'closed' ? `Pull Request  |  Opened by ${creator}  |  ${repo_url}` :
-                    `Pull Request  |  Closed by ${sender}  |  ${repo_url}`),
-            "type": "mrkdwn"
-        }
-    ];
+    // Determine Status and Action summary
+    const readable_action = data.action.replace(/_/g, ' ').replace(/^\w/, c => c.toUpperCase());
+    let status_emoji = pr_status.status_emoji;
+    let action_summary = pr_status.action_summary; // By default, leave it at whatever it was last
+    if (data.pull_request.merged_at) {
+        // Because even on merging, the data.action is still "closed", so we detect it this way
+        action_summary = `Merged by ${sender}`; 
+        status_emoji = ':merged:';
+
+    } else if (['closed', 'opened', 'reopened', 'review_requested'].includes(data.action)) {
+        // Just use those verbs
+        action_summary = `${readable_action} by ${sender}`
+        status_emoji = {
+            'closed': ':x:', 
+            'opened': ':eight_spoked_asterisk:',
+            'reopened': ':eight_spoked_asterisk:',
+            'review_requested': ':raising_hand:'
+        }[data.action];
+
+    } else if (data.review && data.review.state == 'approved') {
+        action_summary = `Approved by ${sender}`;
+        status_emoji = ':white_check_mark:';
+
+    } else if (data.review && data.action == 'submitted') {
+        // Reviewed & not approved 
+        action_summary = `Denied by ${sender}`;
+        status_emoji = ':thumbsdown:';
+    }
+    // Save to persistent memory
+    pr_status.status_emoji = status_emoji;
+    pr_status.action_summary = action_summary;
+    
 
     //Description
-    var description = `_${data.pull_request.additions} Additions, ${data.pull_request.deletions} Deletions_`
+    var description = pr_status.description ?? generateDescription(data);
     if (data.pull_request.body) description = data.pull_request.body
+    pr_status.description = description; // I save this before closed on purpose, to keep the description in case it reopens
     if (data.action == 'closed') description = ''
 
+    
+    pr_status.title = data.pull_request.title;
+
+
+    let button = undefined;
+    if (data.pull_request.state != 'closed' && !(data.review && data.action == 'submitted')) {
+        button = {
+            "type": "button",
+            "text": {
+                "type": "plain_text",
+                "text": ":eyes: Review Pull Request",
+                "emoji": true
+            },
+            "value": `${data.pull_request.id}`,
+            "url": data.pull_request.html_url + '/files',
+            "action_id": "pull_request.add_reviewer"
+        }
+    }
+
     // All Message Blocks
-    const message_blocks = [
+    let message_blocks = [
         {
             "type": "section",
             "text": {
@@ -42,59 +108,75 @@ function generateMessageContent(data) {
                 "text": `<${data.pull_request.html_url}|${status_emoji}  *${data.pull_request.title}*>
 ${description}`
             },
-            "accessory": (data.action != 'closed' ? {
-                "type": "button",
-                "text": {
-                    "type": "plain_text",
-                    "text": ":eyes: Review Pull Request",
-                    "emoji": true
-                },
-                "value": "click_me_123",
-                "url": data.pull_request.html_url,
-                "action_id": "button-action"
-            } : undefined)
+            "accessory": button
         },
         {
             "type": "context",
-            "elements": history_items
+            "elements": [
+                {
+                    "text": `PR by ${creator} |  *${status}*  |  ${action_summary}  |  ${repo_url}`,
+                    "type": "mrkdwn"
+                }
+            ]
         },
         {
             "type": "divider"
         }
     ];
+    if (data.pull_request.merged_at) {
+        message_blocks = [
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "text": `${pr_status.status_emoji}  ${pr_status.title}  |  *${status}*  |  ${repo_url}`,
+                        "type": "mrkdwn"
+                    }
+                ]
+            }
+        ]
+    }
 
+    // Save Presistent PR Status
+    pr_status.message_blocks = message_blocks;
+    prStatuses[data.pull_request.id] = pr_status;
+    await storage.setItem('pr_status', prStatuses)
     return message_blocks;
+}
+
+
+function generateTextForPullRequest (data) {
+    console.log(data)
 }
 
 
 const handlePullRequestEvent = async (data) => {
     // fetch persistent storage
     await storage.init({ dir: './storage/pull_requests' });
-    let messageInfoByPR = await storage.getItem('messages')
-    console.log(messageInfoByPR);
-    
-    console.log(data.action)
+    const messageInfoByPR = await storage.getItem('messages');
+    generateTextForPullRequest(data)
+
     // React to Pull Request notification type
-    if (data.action == 'opened') {
+    if (['opened', 'reopened'].includes(data.action) || !messageInfoByPR[data.pull_request.id]) {
         // Create message
         const result = await sendMessage({
             channel: process.env.DEV_CHAT_CHANNELID, 
-            blocks: generateMessageContent(data)
+            blocks: await generateMessageContentForPullRequest(data)
         })
         messageInfoByPR[data.pull_request.id] = {
             channel: result.channel,
             ts: result.ts
         };
-    } else if (data.action == 'closed' || data.action == 'edited') {
-        // Edit PR message to reflect this
+    } else {
+        // Edit PR message to reflect any changes
         const messageInfo = messageInfoByPR[data.pull_request.id];
-        const result = await updateMessage({
-            ...messageInfo,
-            blocks: generateMessageContent(data)
-        })
+        if (messageInfo) {
+            const result = await updateMessage({
+                ...messageInfo,
+                blocks: await generateMessageContentForPullRequest(data)
+            })
+        }
     }
-
-
 
     await storage.setItem('messages', messageInfoByPR)
 }
